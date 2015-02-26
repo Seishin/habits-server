1465deaeb836e8b1ecc4618037407b27267c48a4
Atanas Dimitrov <seishin90@yandex.ru>
2015-02-26 19:55:42 +0200

    Stats change tests when a habit is incremented.

--- test/habit_test.js
+++ test/habit_test.js
@@ -1,18 +1,25 @@
 require ('./spec_helper')
-var should = require('chai').should()
-var expect = require('chai').expect
-var mongoose = require('mongoose')
+var chai = require('chai')
+var should = chai.should()
+var expect = chai.expect
+var assert = chai.assert 
 
 var When = require('when')
 var Models = require('../lib/models')
 var User = Models.User
 var Habit = Models.Habit
+var UserStats = Models.UserStats
 
 function createUser () {
   var user = new User()
   user.email = "test@test.co"
   user.password = "123"
   user.token = "random_token"
+  
+  var stats = new UserStats()
+  stats.save()
+
+  user.stats = stats
   user.save()
 
   return user
@@ -60,6 +67,16 @@ function updateHabit (id, payload, headers) {
   return Server.injectThen (opts)
 }
 
+function incrementHabit (id, headers) {
+  var opts = {
+    method: 'POST',
+    url: '/habits/increment/' + id,
+    headers: headers
+  }
+
+  return Server.injectThen (opts)
+}
+
 function deleteHabit (id, headers) {
   var opts = {
     method: 'DELETE',
@@ -71,11 +88,6 @@ function deleteHabit (id, headers) {
 }
 
 describe ('Habits', function () {
-  beforeEach(function(done) {
-    if (mongoose.connection.db) return done();
-    mongoose.connect(dbURI, done);
-  });
-
   it ('Should create habit', function (done) {
     var data = {
       text: "Meditation"
@@ -207,7 +219,7 @@ describe ('Habits', function () {
   })
 
   it ('Should update habit', function (done) {
-      var data = {
+    var data = {
       text: "Meditation"
     } 
 
@@ -234,7 +246,7 @@ describe ('Habits', function () {
   })
 
   it ('Should delete habit', function (done) {
-      var data = {
+    var data = {
       text: "Meditation"
     } 
 
@@ -253,5 +265,99 @@ describe ('Habits', function () {
     })
   })
 
+  it ('Should increment habit', function (done) {
+    var data = {
+      text: "Meditation"
+    } 
 
+    When(createUser()).then (function (user) {
+      createHabitResponse = createHabit(data, {authorization: user.token})
+      createHabitResponse.then (function (response) {
+        response.statusCode.should.equal(201)
+        var payload = JSON.parse(response.payload)
+        payload.should.have.length(1)
+
+        incResponse = incrementHabit(payload[0]._id, {authorization: user.token})
+        incResponse.then (function (response) {
+          response.statusCode.should.equal(200)
+          done()
+        })
+      }) 
+    })
+  })
+  
+  it ('Should not increment habit (missing token)', function (done) {
+    var data = {
+      text: "Meditation"
+    } 
+
+    When(createUser()).then (function (user) {
+      createHabitResponse = createHabit(data, {authorization: user.token})
+      createHabitResponse.then (function (response) {
+        response.statusCode.should.equal(201)
+        var payload = JSON.parse(response.payload)
+        payload.should.have.length(1)
+
+        incResponse = incrementHabit(payload[0]._id, {authorization: null})
+        incResponse.then (function (response) {
+          response.statusCode.should.equal(401)
+          done()
+        })
+      }) 
+    })
+  })
+
+  it ('Should not increment habit (wrong token)', function (done) {
+    var data = {
+      text: "Meditation"
+    } 
+
+    When(createUser()).then (function (user) {
+      createHabitResponse = createHabit(data, {authorization: user.token})
+      createHabitResponse.then (function (response) {
+        response.statusCode.should.equal(201)
+        var payload = JSON.parse(response.payload)
+        payload.should.have.length(1)
+
+        incResponse = incrementHabit(payload[0]._id, {authorization: "wrong_token"})
+        incResponse.then (function (response) {
+          response.statusCode.should.equal(401)
+          done()
+        })
+      }) 
+    })
+  })
+
+  it ('Should increment and update user stats', function (done) {
+    var data = {
+      text: "Meditation"
+    } 
+
+    When(createUser()).then (function (user) {
+      createHabitResponse = createHabit(data, {authorization: user.token})
+      createHabitResponse.then (function (response) {
+        var preUserStats = UserStats.findOne({_id: user.stats}).exec()
+
+        response.statusCode.should.equal(201)
+        var payload = JSON.parse(response.payload)
+        payload.should.have.length(1)
+
+        incResponse = incrementHabit(payload[0]._id, {authorization: user.token})
+        incResponse.then (function (response) {
+          response.statusCode.should.equal(200)
+
+          postUserStats = UserStats.findOne({_id: user.stats}).exec()
+
+          When.join(preUserStats, postUserStats).then (function (stats) {
+            assert.isAbove(stats[1].exp, stats[0].exp, 'Post exp > pre exp')
+            assert.isAbove(stats[1].gold, stats[0].gold, 'Post gold > pre gold')
+            assert.isBelow(stats[1].hp, stats[0].hp, 'Post HP == pre HP')
+            stats[1].alive.should.equal(true)
+
+            done()
+          })
+        })
+      }) 
+    })
+  })
 })
