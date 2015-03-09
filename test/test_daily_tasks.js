2751983a46133096000ee4b582ac52b3729dc783
Atanas Dimitrov <seishin90@yandex.ru>
2015-03-09 21:57:58 +0200

    New tests for missing or wrong auth token.

--- test/test_daily_tasks.js
+++ test/test_daily_tasks.js
@@ -27,78 +27,78 @@ function createUser () {
   return user
 }
 
-function createTask(userId) {
+function createTask(userId, token) {
   var opts = {
     method: 'POST',
     url: '/daily-task/?userId=' + userId,
     payload: { text: 'Reading a book 1h' },
-    headers: { authorization: 'random_token' }
+    headers: { authorization: token }
   }
 
   return Server.injectThen (opts)
 }
 
-function getTask (userId, taskId) {
+function getTask (userId, taskId, token) {
   var opts = {
     method: 'GET',
     url: '/daily-task/' + taskId + '/?userId=' + userId,
-    headers: { authorization: 'random_token' }
+    headers: { authorization: token }
   }
 
   return Server.injectThen (opts)
 }
 
-function getAllTasks (userId) {
+function getAllTasks (userId, token) {
   var date = Moment(new Date()).format('YYYY-MM-DD')
   var opts = {
     method: 'GET',
     url: '/daily-task/all/?userId=' + userId + '&date=' + date,
-    headers: { authorization: 'random_token' }
+    headers: { authorization: token }
   }
 
   return Server.injectThen (opts)
 }
 
-function updateTask (userId, taskId) {
+function updateTask (userId, taskId, token, updatedText) {
   var date = Moment(new Date()).format('YYYY-MM-DD')
   var data = { text: "Clean my room" }
   var opts = {
     method: 'PUT',
     url: '/daily-task/' + taskId + '/?userId=' + userId + '&date=' + date,
-    payload: data,
-    headers: { authorization: 'random_token' } 
+    payload: { text: updatedText },
+    headers: { authorization: token } 
   }
 
   return Server.injectThen (opts)
 }
 
-function checkTask (userId, taskId) {
+function checkTask (userId, taskId, token) {
   var date = Moment(new Date()).format('YYYY-MM-DD')
   var opts = {
     method: 'POST',
     url: '/daily-task/' + taskId + '/check/?userId=' + userId + '&date=' + date,
-    headers: { authorization: 'random_token' }
+    headers: { authorization: token }
   }
 
   return Server.injectThen (opts)
 }
 
-function uncheckTask (userId, taskId) {
+function uncheckTask (userId, taskId, token) {
   var date = Moment(new Date()).format('YYYY-MM-DD')
   var opts = {
     method: 'POST',
     url: '/daily-task/' + taskId + '/uncheck/?userId=' + userId + '&date=' + date,
-    headers: { authorization: 'random_token' }
+    headers: { authorization: token }
   }
 
   return Server.injectThen (opts)
 }
 
-function deleteTask (userId, taskId) {
+function deleteTask (userId, taskId, token) {
   var opts = {
     method: 'DELETE',
     url: '/daily-task/' + taskId + '/?userId=' + userId,
-    headers: { authorization: 'random_token' } 
+    headers: { authorization: token } 
   }
 
   return Server.injectThen (opts)
@@ -117,46 +117,77 @@ describe ('Daily Task', function () {
   })
 
   it ('Should create a daily task', function (done) {
-    request = createTask(user._id)
-    When(request).then (function (response) {
+    request = createTask(user._id, user.token)
+    request.then (function (response) {
       payload = JSON.parse(response.payload)
 
       response.statusCode.should.equal(201)
-      payload.should.have.property("text")
-      payload.should.have.property("user")
+      payload.should.have.property('text')
+      payload.should.have.property('user')
       taskId = payload._id
       done()
     })
   })
-    
+
+  it ('Should not create a daily task (missing token)', function (done) {
+    request = createTask(user._id, null)
+    request.then (function (response) {
+      response.statusCode.should.equal(401)
+      done()
+    })
+  })
+
+ it ('Should not create a daily task (wrong token)', function (done) {
+    request = createTask(user._id, 'wrong_token')
+    request.then (function (response) {
+      response.statusCode.should.equal(401)
+      done()
+    })
+  })
+   
   describe ('Manipulations', function () {
     task = null
 
     beforeEach (function (done) {
-      request = createTask(user._id)
-      When(createTask(user._id)).then (function (result) {
+      When(createTask(user._id, user.token)).then (function (result) {
         if (result != null)
           task = JSON.parse(result.payload)
           done()
       })
     })
 
-    it ('Should get a specific daily task', function (done) {
-      request = getTask(user._id, task._id)
-      When(request).then (function (response) { 
+    it ('Should get a daily task', function (done) {
+      request = getTask(user._id, task._id, user.token)
+      request.then (function (response) { 
         payload = JSON.parse(response.payload)
 
         response.statusCode.should.equal(200)
-        payload.should.have.property("text")
-        payload.should.have.property("user")
-        payload.should.have.property("state")
+        payload.should.have.property('text')
+        payload.should.have.property('user')
+        payload.should.have.property('state')
         done() 
+      })
+    })
+    
+    it ('Should not get a daily task (missing token)', function (done) {
+      request = getTask(user._id, task._id, null)
+      request.then (function (response) { 
+        response.statusCode.should.equal(401)
+        done()
+      })
+    })
+    
+    it ('Should not get a daily task (wrong token)', function (done) {
+      request = getTask(user._id, task._id, 'wrong_tokne')
+      request.then (function (response) { 
+        response.statusCode.should.equal(401)
+        done()
       })
     })
 
     it ('Should get all daily tasks', function (done) {
-      request = getAllTasks(user._id, task._id)
-      When(request).then (function (response) {
+      request = getAllTasks(user._id, user.token)
+      request.then (function (response) {
         payload = JSON.parse(response.payload)
 
         response.statusCode.should.equal(200)
@@ -165,40 +196,88 @@ describe ('Daily Task', function () {
         done()
       })
     })
+    
+    it ('Should not get all daily tasks (missing token)', function (done) {
+      request = getAllTasks(user._id, null)
+      request.then (function (response) {
+        response.statusCode.should.equal(401)
+        done()
+      })
+    })
 
-    it ('Should check a specific task', function (done) {
-      request = checkTask(user._id, task._id)
-      When(request).then (function (response) {
+    it ('Should not get all daily tasks (wrong token)', function (done) {
+      request = getAllTasks(user._id, 'wrong_token')
+      request.then (function (response) {
+        response.statusCode.should.equal(401)
+        done()
+      })
+    })
+
+    it ('Should check a task', function (done) {
+      request = checkTask(user._id, task._id, user.token)
+      request.then (function (response) {
         payload = JSON.parse(response.payload)
 
         response.statusCode.should.equal(200)
-        payload.should.have.property("text")
-        payload.should.have.property("user")
-        payload.should.have.property("state")
+        payload.should.have.property('text')
+        payload.should.have.property('user')
+        payload.should.have.property('state')
         done()
       }) 
     })
 
-    it ('Should uncheck a specific task', function (done) {
-      request = checkTask(user._id, task._id)
-      When(request).then (function (response) {
-        request = uncheckTask(user._id, task._id)
-        When(request).then (function (response) {
+    it ('Should not check a task (missing token)', function (done) {
+      request = checkTask(user._id, task._id, null)
+      request.then (function (response) {
+        response.statusCode.should.equal(401)
+        done()
+      })
+    })
+
+    it ('Should not check a task (wrong token)', function (done) {
+      request = checkTask(user._id, task._id, 'wrong_token')
+      request.then (function (response) {
+        response.statusCode.should.equal(401)
+        done()
+      })
+    })
+
+    it ('Should uncheck a task', function (done) {
+      request = checkTask(user._id, task._id, user.token)
+      request.then (function (response) {
+        request = uncheckTask(user._id, task._id, user.token)
+        request.then (function (response) {
           payload = JSON.parse(response.payload)
 
           response.statusCode.should.equal(200)
-          payload.should.have.property("text")
-          payload.should.have.property("user")
-          payload.should.have.property("state")
+          payload.should.have.property('text')
+          payload.should.have.property('user')
+          payload.should.have.property('state')
           done()
         }) 
       })
     })
 
+    it ('Should not uncheck a task (missing token)', function (done) {
+      request = checkTask(user._id, task._id, null)
+      request.then (function (response) {
+        response.statusCode.should.equal(401)
+        done()
+      })
+    })
 
-    it ('Should check a specific task and update user\'s stats', function (done) {
+    it ('Should not uncheck a task (wrong token)', function (done) {
+      request = checkTask(user._id, task._id, 'wrong_token')
+      request.then (function (response) {
+        response.statusCode.should.equal(401)
+        done()
+      })
+    })
+
+    it ('Should check a task and update user\'s stats', function (done) {
       preUserStats = UserStats.findOne({_id: user.stats}).exec()
-      request = checkTask(user._id, task._id)
+
+      request = checkTask(user._id, task._id, user.token)
       request.then (function (response) {
         response.statusCode.should.equal(200)
 
@@ -216,10 +295,10 @@ describe ('Daily Task', function () {
     }) 
 
     it ('Should uncheck a specific task and update user\'s stats', function (done) {
-      request = checkTask(user._id, task._id)
+      request = checkTask(user._id, task._id, user.token)
       request.then (function (response) {
         preUserStats = UserStats.findOne({_id: user.stats}).exec()
-        request = uncheckTask(user._id, task._id)
+        request = uncheckTask(user._id, task._id, user.token)
         request.then (function (response) {
           response.statusCode.should.equal(200)
 
@@ -237,23 +316,29 @@ describe ('Daily Task', function () {
       })
     })
 
-    it ('Should update a specific task', function (done) {
+    it ('Should update a task', function (done) {
       preUpdateText = task.text
-      request = updateTask(user._id, task._id)
+      updatedText = 'Meditation 1h'
+
+      request = updateTask(user._id, task._id, user.token, updatedText)
       request.then (function (response) {
         response.statusCode.should.equal(200)
+
         payload = JSON.parse(response.payload)
+        payload.should.have.property('text')
+        payload.should.have.property('state')
         payload.text.should.not.equal(preUpdateText)
+
         done()
       })
     })
 
     it ('Should delete a specific task', function (done) {
-      request = deleteTask(user._id, task._id)
+      request = deleteTask(user._id, task._id, user.token)
       request.then (function (response) {
         response.statusCode.should.equal(200)
         payload = JSON.parse(response.payload)
-        payload.should.have.property("id")
+        payload.should.have.property('id')
         done()
       })
     })
