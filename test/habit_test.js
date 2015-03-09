f4284f2484e8539763fc6a03edc3b5358e9ac1c4
Atanas Dimitrov <seishin90@yandex.ru>
2015-03-09 21:13:30 +0200

    Habits test refactored.

--- test/habit_test.js
+++ test/habit_test.js
@@ -12,67 +12,73 @@ var Habit = Models.Habit
 var UserStats = Models.UserStats
 
 function createUser () {
-  var user = new User()
+  user = new User()
   user.email = "test@test.co"
   user.password = "123"
   user.token = "random_token"
   
-  var stats = new UserStats()
+  stats = new UserStats()
   stats.save()
 
   user.stats = stats
   user.save()
-
-  return user
+  
+  When.all(user, stats).then (function (result) {
+    return result[0] 
+  })
 }
 
-function createHabit (userId, payload, token) {
+function createHabit (userId, token) {
   var opts = {
     method: 'POST',
     url: '/habits/?userId=' + userId,
-    payload: payload,
-    headers: {authorization: token} 
+    payload: { text: 'Meditation 30 min' },
+    headers: {authorization: token } 
   }
 
   return Server.injectThen (opts)
 }
 
-function getHabit (userId, habitId, token, date) {
+function getHabit (userId, habitId, token) {
+  var date = Moment(new Date()).format('YYYY-MM-DD')
   var opts = {
     method: 'GET',
     url: '/habits/' + habitId + '/?userId=' + userId + '&date=' + date,
-    headers: {authorization: token} 
+    headers: {authorization: token } 
   }
 
   return Server.injectThen (opts)
 }
 
-function getAllHabit (userId, date, token) {
+function getAllHabits (userId, token) {
+  var date = Moment(new Date()).format('YYYY-MM-DD')
   var opts = {
     method: 'GET',
     url: '/habits/all/?userId=' + userId + '&date=' + date,
-    headers: {authorization: token} 
+    headers: {authorization: token } 
   }
 
   return Server.injectThen (opts)
 }
 
-function updateHabit (userId, habitId, payload, token, date) {
+function updateHabit (userId, habitId, token) {
+  var date = Moment(new Date()).format('YYYY-MM-DD')
   var opts = {
     method: 'PUT',
     url: '/habits/' + habitId + '/?userId=' + userId + '&date=' + date,
-    headers: {authorization: token},
-    payload: payload
+    headers: { authorization: token },
+    payload: { text: 'Reading a book 1h' } 
   }
 
   return Server.injectThen (opts)
 }
 
-function incrementHabit (userId, habitId, date, token) {
+function incrementHabit (userId, habitId, token) {
+  var date = Moment(new Date()).format('YYYY-MM-DD')
   var opts = {
     method: 'POST',
     url: '/habits/increment/' + habitId + '/?userId=' + userId + '&date=' + date,
-    headers: {authorization: token} 
+    headers: {authorization: token } 
   }
 
   return Server.injectThen (opts)
@@ -82,308 +88,182 @@ function deleteHabit (userId, habitId, token) {
   var opts = {
     method: 'DELETE',
     url: '/habits/' + habitId + '/?userId=' + userId,
-    headers: {authorization: token},
+    headers: {authorization: token },
   }
 
   return Server.injectThen (opts)
 }
 
 describe ('Habits', function () {
-  it ('Should create habit', function (done) {
-    var data = {
-      text: "Meditation"
-    }
-
-    When(createUser()).then (function (user) {
-      response = createHabit(user._id, data, user.token)
-      response.then (function (response) {
-        response.statusCode.should.equal(201)
-        payload = JSON.parse(response.payload)
-        
-        payload.should.have.property("state")
-        done()
-      })
-    })
-  })
-
-  it ('Should not create habit (missing token)', function (done) {
-    var data = {
-      text: "Meditation"
-    }
-
-    When(createUser()).then (function (user) {
-      response = createHabit(user._id, data, null)
-      response.then (function (response) {
-        response.statusCode.should.equal(401)
-        done()
-      }) 
-    })
-  })
-
-  it ('Should not create habit (wrong token)', function (done) {
-    var data = {
-      text: "Meditation"
-    }
-
-    When(createUser()).then (function (user) {
-      response = createHabit(user._id, data, "wrong_token")
-      response.then (function (response) {
-        response.statusCode.should.equal(401)
-        done()
-      }) 
-    })
-  })
-
-  it ('Should get all habits', function (done) {
-    var data = {
-      text: "Meditation"
-    } 
-
-    When(createUser()).then (function (user) {
-      createHabitResponse = createHabit(user._id, data, user.token)
-      createHabitResponse.then (function (response) {
-        response.statusCode.should.equal(201)
-        
-        date = Moment(new Date()).format('YYYY-MM-DD')
-
-        getResponse = getAllHabit(user._id, date, user.token)
-        getResponse.then (function (response) {
-          var payload = JSON.parse(response.payload)
-          response.statusCode.should.equal(200)
-          payload.should.have.property("habits")
-
-          done() 
-        })
-      }) 
-    })
-  })
-
-  it ('Should get habit', function (done) {
-    var data = {
-      text: "Meditation"
-    } 
-
-    When(createUser()).then (function (user) {
-      createHabitResponse = createHabit(user._id, data, user.token)
-      createHabitResponse.then (function (response) {
-        response.statusCode.should.equal(201)
-        payload = JSON.parse(response.payload)
-        
-        date = Moment(new Date()).format('YYYY-MM-DD')
-
-        getResponse = getHabit(user._id, payload._id, user.token, date)
-        getResponse.then (function (response) {
-          response.statusCode.should.equal(200)
-          payload = JSON.parse(response.payload)
-          payload.should.have.property("text")
-          payload.text.should.equal(data.text)
-          
-          payload.should.have.property("state")
-          payload.state.should.equal(0)
-          done()
-        })
-      }) 
-    })
-  })
-
-  it ('Should not get habit (missing token)', function (done) {
-      var data = {
-      text: "Meditation"
-    } 
-
-    When(createUser()).then (function (user) {
-      createHabitResponse = createHabit(user._id, data, user.token)
-      createHabitResponse.then (function (response) {
-        response.statusCode.should.equal(201)
-        payload = JSON.parse(response.payload)
-        
-        date = Moment(new Date()).format('YYYY-MM-DD')
-
-        getResponse = getHabit(user._id, payload._id, null, date)
-        getResponse.then (function (response) {
-          response.statusCode.should.equal(401)
-          done()
-        })
-      }) 
-    })
-  })
-
-  it ('Should not get habit (wrong token)', function (done) {
-      var data = {
-      text: "Meditation"
-    } 
-
-    When(createUser()).then (function (user) {
-      createHabitResponse = createHabit(user._id, data, user.token)
-      createHabitResponse.then (function (response) {
-        response.statusCode.should.equal(201)
-        var payload = JSON.parse(response.payload)
-
-        date = Moment(new Date()).format('YYYY-MM-DD')
-        
-        getResponse = getHabit(user._id, payload._id, 'wrong_token', date)
-        getResponse.then (function (response) {
-          response.statusCode.should.equal(401)
-          done()
-        })
-      }) 
-    })
-  })
-
-  it ('Should update habit', function (done) {
-    var data = {
-      text: "Meditation"
-    } 
-
-    When(createUser()).then (function (user) {
-      createHabitResponse = createHabit(user._id, data, user.token)
-      createHabitResponse.then (function (response) {
-        response.statusCode.should.equal(201)
-        var payload = JSON.parse(response.payload)
-        
-        var newData = {
-          text: "Reading a book",
-          state: 0
-        }
-
-        date = Moment(new Date()).format('YYYY-MM-DD')
-
-        getResponse = updateHabit(payload.user, payload._id, newData, user.token, date)
-        getResponse.then (function (response) {
-          var payload = JSON.parse(response.payload)
-          response.statusCode.should.equal(200)
-          payload.should.have.property("text")
-          payload.text.should.equal(newData.text)
-          payload.should.have.property("state")
-          done()
-        })
-      }) 
-    })
-  })
-
-  it ('Should delete habit', function (done) {
-    var data = {
-      text: "Meditation"
-    } 
-
-    When(createUser()).then (function (user) {
-      createHabitResponse = createHabit(user._id, data, user.token)
-      createHabitResponse.then (function (response) {
-        var payload = JSON.parse(response.payload)
-        response.statusCode.should.equal(201)
-        
-        getResponse = deleteHabit(payload.user, payload._id, user.token)
-        getResponse.then (function (response) {
-          response.statusCode.should.equal(200)
-          payload = JSON.parse(response.payload)
-
-          payload.should.have.property("id")
-          done()
-        })
-      }) 
-    })
-  })
-
-  it ('Should increment habit', function (done) {
-    var data = {
-      text: "Meditation"
-    } 
-
-    When(createUser()).then (function (user) {
-      createHabitResponse = createHabit(user._id, data, user.token)
-      createHabitResponse.then (function (response) {
-        response.statusCode.should.equal(201)
-        var payload = JSON.parse(response.payload)
-
-        date = Moment(new Date()).format('YYYY-MM-DD')
-
-        incResponse = incrementHabit(user._id, payload._id, date, user.token)
-        incResponse.then (function (response) {
-          response.statusCode.should.equal(200)
-          payload = JSON.parse(response.payload)
-          payload.should.have.property("text")
-          payload.text.should.equal(data.text)
-          
-          payload.should.have.property("state")
-          payload.state.should.equal(0)
-
-          done()
-        })
-      }) 
+  user = null
+  taskId = null
+  
+  beforeEach(function (done) {
+    When(createUser()).then (function (result) {
+      if (result != null)
+        user = result
+        done()   
     })
   })
   
-  it ('Should not increment habit (missing token)', function (done) {
-    var data = {
-      text: "Meditation"
-    } 
-
-    When(createUser()).then (function (user) {
-      createHabitResponse = createHabit(user._id, data, user.token)
-      createHabitResponse.then (function (response) {
-        response.statusCode.should.equal(201)
-        var payload = JSON.parse(response.payload)
-
-        incResponse = incrementHabit(payload.user, payload._id, null)
-        incResponse.then (function (response) {
-          response.statusCode.should.equal(401)
-          done()
-        })
-      }) 
+  it ('Should create habit', function (done) {
+    request = createHabit(user._id, user.token)
+    request.then (function (response) {
+      response.statusCode.should.equal(201)
+      payload = JSON.parse(response.payload)
+      payload.should.have.property('text')
+      payload.should.have.property('state')
+      done()
     })
   })
-
-  it ('Should not increment habit (wrong token)', function (done) {
-    var data = {
-      text: "Meditation"
-    } 
-
-    When(createUser()).then (function (user) {
-      createHabitResponse = createHabit(user._id, data, user.token)
-      createHabitResponse.then (function (response) {
-        response.statusCode.should.equal(201)
-        var payload = JSON.parse(response.payload)
-
-        incResponse = incrementHabit(payload.user, payload._id, "wrong_token")
-        incResponse.then (function (response) {
-          response.statusCode.should.equal(401)
-          done()
-        })
-      }) 
-    })
+  
+  it ('Should not create habit (missing token)', function (done) {
+    request = createHabit(user._id, null)
+    request.then (function (response) {
+      response.statusCode.should.equal(401)
+      done()
+    }) 
   })
 
-  it ('Should increment and update user stats', function (done) {
-    var data = {
-      text: "Meditation"
-    } 
+  it ('Should not create habit (wrong token)', function (done) {
+    request = createHabit(user._id, 'wrong_token')
+    request.then (function (response) {
+      response.statusCode.should.equal(401)
+      done()
+    }) 
+  })
 
-    When(createUser()).then (function (user) {
-      createHabitResponse = createHabit(user._id, data, user.token)
-      createHabitResponse.then (function (response) {
-        var preUserStats = UserStats.findOne({_id: user.stats}).exec()
+  describe ('Manipulations', function () {
+    habit = null
 
-        response.statusCode.should.equal(201)
-        var payload = JSON.parse(response.payload)
+    beforeEach (function (done) {
+      request = createHabit(user._id, user.token)
+      request.then (function (response) {
+        if (response != null)
+          habit = JSON.parse(response.payload)
+          done()
+      })
+    })
 
-        date = Moment(new Date()).format('YYYY-MM-DD')
+    it ('Should get all habits', function (done) {
+      request = getAllHabits(user._id, user.token)
+      request.then (function (response) {
+        payload = JSON.parse(response.payload)
 
-        incResponse = incrementHabit(user._id, payload._id, date, user.token)
-        incResponse.then (function (response) {
-          response.statusCode.should.equal(200)
+        response.statusCode.should.equal(200)
+        payload.should.have.property("habits")
+        assert(Array.isArray(payload.habits), 'habits property is an array')
+        assert.isAbove(payload.habits.length, 0, 'habits array lenght must be > 1')
+        done() 
+      })
+    })
 
-          postUserStats = UserStats.findOne({_id: user.stats}).exec()
-
-          When.join(preUserStats, postUserStats).then (function (stats) {
-            assert.isAbove(stats[1].exp, stats[0].exp, 'Post exp > pre exp')
-            assert.isAbove(stats[1].gold, stats[0].gold, 'Post gold > pre gold')
-            assert.equal(stats[1].hp, stats[0].hp, 'Post HP == pre HP')
-            stats[1].alive.should.equal(true)
-
-            done()
-          })
-        })
+    it ('Should get habit', function (done) {
+      request = getHabit(user._id, habit._id, user.token)
+      request.then (function (response) {
+        payload = JSON.parse(response.payload)
+        
+        response.statusCode.should.equal(200)
+        payload.should.have.property("text")
+        payload.should.have.property("state")
+        payload.state.should.equal(0)
+        done()
       }) 
+    })
+
+    it ('Should not get habit (missing token)', function (done) {
+      request = getHabit(user._id, habit._id, null)
+      request.then (function (response) {
+        response.statusCode.should.equal(401)
+        done()
+      })
+    })
+
+    it ('Should not get habit (wrong token)', function (done) {
+      request = getHabit(user._id, habit._id, 'wrong_token')
+      request.then (function (response) {
+        response.statusCode.should.equal(401)
+        done()
+      })
+    })
+
+    it ('Should update habit', function (done) {
+      request = updateHabit(user._id, habit._id, user.token)
+      request.then (function (response) {
+        payload = JSON.parse(response.payload)
+
+        response.statusCode.should.equal(200)
+        payload.should.have.property('text')
+        payload.should.have.property('state')
+        done()
+      })
+    })
+
+    it ('Should delete habit', function (done) {
+      request = deleteHabit(user._id, habit._id, user.token)
+      request.then (function (response) {
+        response.statusCode.should.equal(200)
+        payload = JSON.parse(response.payload)
+
+        payload.should.have.property("id")
+        done()
+      })
+    })
+
+    it ('Should increment habit', function (done) {
+      request = incrementHabit(user._id, habit._id, user.token)
+      request.then (function (response) {
+        payload = JSON.parse(response.payload)
+        
+        response.statusCode.should.equal(200)
+        payload.should.have.property('text')
+        payload.should.have.property('state')
+        payload.state.should.equal(0)
+
+        done()
+      })
+    }) 
+    
+    it ('Should not increment habit (missing token)', function (done) {
+      request = incrementHabit(user._id, habit._id, null)
+      request.then (function (response) {
+        response.statusCode.should.equal(401)
+        done()
+      })
+    }) 
+
+    it ('Should not increment habit (wrong token)', function (done) {
+      request = incrementHabit(user._id, habit._id, 'wrong_token')
+      request.then (function (response) {
+        response.statusCode.should.equal(401)
+        done()
+      })
+    })
+
+    it ('Should increment and update user stats', function (done) {
+      preUserStats = UserStats.findOne({_id: user.stats}).exec()
+      
+      request = incrementHabit(user._id, habit._id, user.token)
+      request.then (function (response) {
+        response.statusCode.should.equal(200)
+
+        postUserStats = UserStats.findOne({_id: user.stats}).exec()
+
+        When.join(preUserStats, postUserStats).then (function (stats) {
+          assert.isAbove(stats[1].exp, stats[0].exp, 'Post exp > pre exp')
+          assert.isAbove(stats[1].gold, stats[0].gold, 'Post gold > pre gold')
+          assert.equal(stats[1].hp, stats[0].hp, 'Post HP == pre HP')
+          stats[1].alive.should.equal(true)
+
+          done()
+        })
+      })
+    }) 
+  })
+
+  after(function(done) {
+    clearDB(function(err) {
+      done()
     })
   })
 })
