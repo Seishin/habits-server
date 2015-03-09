3a228ea7de221ad54bdd1921a324fa9e09df754d
Atanas Dimitrov <seishin90@yandex.ru>
2015-03-09 18:53:35 +0200

    UserStats test refactored.

--- test/user_stats_test.js
+++ test/user_stats_test.js
@@ -7,14 +7,21 @@ var Models = require('../lib/models')
 var UserStats = Models.UserStats
 var User = Models.User
 
-function createUser (payload) {
-  var opts = {
-    method: 'POST',
-    url: '/users',
-    payload: payload
-  }
+function createUser () {
+  user = new User()
+  user.email = "test@test.co"
+  user.password = "123"
+  user.token = "random_token"
+  
+  stats = new UserStats()
+  stats.save()
 
-  return Server.injectThen(opts)
+  user.stats = stats
+  user.save()
+  
+  When.all(user, stats).then (function (result) {
+    return result[0] 
+  })
 }
 
 function getUserStats(token, userId) {
@@ -28,39 +35,34 @@ function getUserStats(token, userId) {
 }
 
 describe ('UserStats', function() {
-  afterEach(function(done) {
+  user = null
+  
+  beforeEach(function (done) {
+    When(createUser()).then (function (result) {
+      if (result != null)
+        user = result
+        done()   
+    })
+  })
+
+  it ('Should get user\'s privete stats', function (done) {
+    request = getUserStats(user.token, user._id)
+    request.then (function (response) {
+      response.statusCode.should.equal(200)
+      payload = JSON.parse(response.payload)
+      payload.should.have.property("hp")
+      payload.should.have.property("exp")
+      payload.should.have.property("gold")
+      payload.should.have.property("lvl")
+      payload.should.have.property("minLvlExp")
+      payload.should.have.property("maxLvlExp")
+      done()
+    })
+  })  
+
+  after(function(done) {
     clearDB(function(err) {
       done()
     })
   })
-
-  it ('Should get user\'s stats', function (done) {
-    var data = {
-      email: 'test@test.co',
-      password: '123',
-      name: "test_name"
-    }
-
-    createUserResponse = createUser(data)
-    createUserResponse.then (function (response) {
-      response.statusCode.should.equal(201)
-      var payload = JSON.parse(response.payload)
-      payload.should.have.property('token')
-
-      getStatsResponse = getUserStats(payload.token, payload._id)
-      getStatsResponse.then (function (response) {
-        response.statusCode.should.equal(200)
-        payload = JSON.parse(response.payload)
-        payload.should.have.property("hp")
-        payload.should.have.property("exp")
-        payload.should.have.property("gold")
-        payload.should.have.property("lvl")
-        payload.should.have.property("minLvlExp")
-        payload.should.have.property("maxLvlExp")
-        done()
-      })
-    })
-  })  
-
-
 })
