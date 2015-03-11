6a467a232fc706baef00aa83ecdd03494d8ef98b
Atanas Dimitrov <seishin90@yandex.ru>
2015-03-11 20:07:02 +0200

    Removed clearDb function.

--- test/utils/cleardb.js
+++ test/utils/cleardb.js
@@ -1,8 +1,2 @@
 var dbURI = 'mongodb://localhost/habits'
-global.clearDB = require('mocha-mongoose')(dbURI)
-
-afterEach(function(done) {
-  clearDB(function(err) {
-    done()
-  })
-})
+clearDB = require('mocha-mongoose')(dbURI)
